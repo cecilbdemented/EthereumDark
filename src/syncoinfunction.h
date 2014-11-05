9c4a7bf4ec0039158971d07b55111c8bf7d2a008
Ebolacoin <etdethereum@gmail.com>
2014-11-05 15:07:56 -0600

    First Upload

---
+++ src/syncoinfunction.h
@@ -0,0 +1,35 @@
+/* 
+
+ */
+
+#ifndef ETDOINFUNCTION_H
+#define	ETDOINFUNCTION_H
+
+double getHardness();
+double GetEstimatedNextHardness();
+double getReward();
+double getAcreHardness(int);
+double getTxTotalValue(std::string);
+double convertCoins(int);
+double getTxFees(std::string);
+
+int getTotalVolume();
+int getNetworkPawsPS();
+int getAcreTime(int);
+int getAcrenBits(int);
+int getAcreNonce(int);
+int acresInPastHours(int);
+int getAcrePawrate(int);
+
+std::string getNodeInfo();
+std::string getInputs(std::string);
+std::string getOutputs(std::string);
+std::string getAcreHash(int);
+std::string getAcreMerkle(int);
+
+bool addnode(std::string);
+
+const CBlockIndex* getAcreIndex(int);
+
+int getInputValue(CTransaction, CScript);
+#endif
